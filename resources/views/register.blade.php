661b855973decb868cdec094eeb4f2c20c840a85
KohakuCao <kohaku@hepnovel.com>
2023-02-27 00:15:55 +0800

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh-CN">
 <head>
 	@include('components.header')
-	<title>注册 - THParty</title>
+	<title>注册 - THParty.Fun</title>
 </head>
 <body>
 <div id="bgCover"></div>
@@ -12,7 +12,7 @@
 		<div class="card shadow-lg col-12 col-md-9 col-lg-6 opacity-75">
 			<div class="card-body">
 				<h4 class="card-title my-4 text-center">注册</h4>
-				<form method="post" id="loginForm" action="/Action/Login">
+				<form method="post" id="regForm" action="/Actions/Register">
 					@csrf
 					<div class="input-group mb-3">
 						<span class="input-group-text" id="basic-addon1">+86</span>
@@ -37,7 +37,7 @@
 					<button onclick="$('#loginForm').submit();" class="btn btn-outline-primary col-6">注册</button>
 				</div>
 				<div class="row justify-content-around px-2 mt-4 text-center">
-					<a href="#" class="text-muted">已有账号？点此登录</a>
+					<a href="/Login" class="text-muted">已有账号？点此登录</a>
 				</div>
 				<div class="px-2 my-4">
 					@foreach($errors->all() as $error) <p class="text-center text-danger" id="warning">{{$error}}</p>@endforeach
