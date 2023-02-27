139884255307c2c6dac463541e665a96f7737f10
李泊桥 <13693338025@163.com>
2023-02-27 15:23:13 +0800

    Add login.blade.php and change register.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh-CN">
 <head>
 	@include('components.header')
-	<title>注册 - THParty.Fun</title>
+	<title>登录 - THParty.Fun</title>
 </head>
 <body>
 <div id="bgCover"></div>
@@ -11,34 +11,25 @@
 	<div class="row justify-content-center">
 		<div class="card shadow-lg col-12 col-md-9 col-lg-6 opacity-75">
 			<div class="card-body">
-				<h4 class="card-title my-4 text-center">注册</h4>
-				<form method="post" id="regForm" action="/Actions/Register">
+				<h4 class="card-title my-4 text-center">登录</h4>
+				<form method="post" id="loginForm" action="/Actions/Login">
 					@csrf
-					<div class="input-group mb-3">
-						<span class="input-group-text" id="basic-addon1">+86</span>
-						<input type="text" class="form-control" name="phone" id="phone" placeholder="手机号码" onkeydown="sub(event);">
-					</div>
 					<div class="mb-3">
 						<input type="text" class="form-control" name="username" id="username" placeholder="用户名" onkeydown="sub(event);">
 					</div>
 					<div class="mb-3">
 						<input type="password" class="form-control" name="password" id="password" placeholder="密码" onkeydown="sub(event);">
 					</div>
-					<div class="input-group mb-3">
-						<input type="text" class="form-control" name="captcha" id="captcha" placeholder="手机验证码" onkeydown="sub(event);">
-						<button class="btn btn-outline-success" type="button" id="button-addon2">发送验证码</button>
-					</div>
+
 					<div class="mb-3 form-check">
 						<input type="checkbox" class="form-check-input" id="remember" name="remember">
 						<label class="form-check-label" for="remember">记住我</label>
 					</div>
 				</form>
 				<div class="row justify-content-around px-2 mt-4">
-					<button onclick="$('#regForm').submit();" class="btn btn-outline-primary col-6">注册</button>
+					<button onclick="$('#loginForm').submit();" class="btn btn-outline-primary col-6">登录</button>
 				</div>
-				<div class="row justify-content-around px-2 mt-4 text-center">
-					<a href="/Login" class="text-muted">已有账号？点此登录</a>
-				</div>
+
 				<div class="px-2 my-4">
 					@foreach($errors->all() as $error) <p class="text-center text-danger" id="warning">{{$error}}</p>@endforeach
 				</div>
