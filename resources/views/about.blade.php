9db9ff37e060b678ef6c1eac4440bae1a1341de1
KohakuCao <kohaku@hepnovel.com>
2023-03-06 22:53:58 +0800

    寄吧

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -62,7 +62,7 @@
 				<div class="card-body">
 					<figure class="my-4 px-3">
 						<blockquote class="blockquote">
-							<p>Here’s to the crazy ones, the misfits, the rebels, the troublemakers, the round pegs in the square holes … the ones who see things differently — they’re not fond of rules, and they have no respect for the status quo. … You can quote them, disagree with them, glorify or vilify them, but the only thing you can’t do is ignore them because they change things. … They push the human race forward, and while some may see them as the crazy ones, we see genius, because the people who are crazy enough to think that they can change the world, are the ones who do.</p>
+							<p><small>Here’s to the crazy ones, the misfits, the rebels, the troublemakers, the round pegs in the square holes … the ones who see things differently — they’re not fond of rules, and they have no respect for the status quo. … You can quote them, disagree with them, glorify or vilify them, but the only thing you can’t do is ignore them because they change things. … They push the human race forward, and while some may see them as the crazy ones, we see genius, because the people who are crazy enough to think that they can change the world, are the ones who do.</small></p>
 						</blockquote>
 						<figcaption class="blockquote-footer">
 							Steve Jobs
