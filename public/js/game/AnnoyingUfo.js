22772a1366e749d2418df2d22c449a8b414227c5
李泊桥 <13693338025@163.com>
2023-04-09 21:46:51 +0800

    UFO增加timer

--- public/js/game/AnnoyingUfo.js
+++ public/js/game/AnnoyingUfo.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
 	var groupNum = 1;
+	$("#deleteGroup1").hide();
+	$("#groupList").on("click",".deleteGroup",function (){
+		tid = $(this).attr("tid");
+		// alert(tid);
+		$("div[id=groupItem"+tid+"]").fadeOut();//非常不好的操作，可能会给未来留下大坑！
+	});
 	$("#groupList").on("blur",".groupName",function(){
 		$(this).removeClass("form-control");
 		$(this).addClass("form-control-plaintext");
+		tid = $(this).attr("tid");
+		var t=1;
+		var countDown = setInterval(function () {
+			if (t == 0) {
+				// doing some alarm;
+				$("button[id=deleteGroup"+tid+"]").fadeOut();
+				clearInterval(countDown);
+				return 0;
+			}
+			t--;
+		}, 100);
 	});
+
 	$("#groupList").on("click",".groupName",function(){
 		$(this).addClass("form-control");
 		$(this).removeClass("form-control-plaintext");
+		tid = $(this).attr("tid");
+		$("button[id=deleteGroup"+tid+"]").fadeIn("fast");
 	});
 	$("#groupList").on("click",".scoreMinus",function(){
 		tid = $(this).attr("tid");
@@ -18,17 +38,91 @@ $(document).ready(function(){
 		tid = $(this).attr("tid");
 		$("#score" + tid.toString()).html(parseInt($("#score" + tid.toString()).html()) + 1);
 	});
+
 	$("#addGroup").click(function(){
 		groupNum++;
 		tid = groupNum.toString();
 		var newGroup = "<div class=\"list-group-item row py-3 lh-tight\" id=\"groupItem"+tid+"\">\n" +
-			"                        <div class='row justify-content-center'><div class='col-8'><input type=\"text\" class=\"groupName col-7 form-control\" placeholder='队伍名称...' id=\'groupName\'"+tid+"></div>\n" +
-			"                        <div class=\"groupCount col-4 justify-content-center\">\n" +
+			"                        <div class='row justify-content-center'><div class='col-8'><input type=\"text\" class=\"groupName col-7 form-control\" placeholder='队伍名称...' id=\'groupName"+tid+"\' tid=\'"+tid+"\'></div>\n" +
+			"                        <div class=\"groupCount col-3 justify-content-center\">\n" +
 			"                            <button class=\"btn scoreMinus btn-danger\" id=\"scoreMinus1\" tid=\""+tid+"\"><h5 class='d-inline'>-</h5></button>\n" +
 			"                            <span><b class=\"score\" id=\"score"+tid+"\">0</b></span>\n    " +
 			"                            <button class=\"btn scoreAdd btn-success\" id=\"scoreAdd1\" tid=\""+tid+"\"><h5 class='d-inline'>+</h5></button>\n" +
-			"                        </div></div>\n" +
+			"                        </div><div class=\"col-1\"><button class=\"btn deleteGroup\" id=\"deleteGroup"+tid+"\" tid=\""+tid+'\" display=\"none\"><i class="bi bi-x-circle"></i></button></div>'+"\n"+
 			"                    </div>";
+
 		$("#groupItem"+(groupNum-1).toString()).after(newGroup);
+		$("#groupName"+tid).focus();
 	});
+
+
+
+
+	$("#timerSet").hide();
+	var timeCount = 15;
+	var countDown;
+	var isCounting = 0;
+	$("#timerChange").on("click",function (){
+		if($(this).attr("setting")==='1'){
+			$(this).attr("setting","0");
+			// $(this).addClass("btn-success");
+			// $(this).removeClass("btn-outline-dark");
+			$("#timerSet").fadeIn("fast");
+			$(this).html("<i class=\"bi bi-check-lg\"></i>");
+		}else if($(this).attr("setting")==='0'){
+			$("#timerSet").fadeOut();
+			$(this).attr("setting","1");
+			// $(this).removeClass("btn-success");
+			// $(this).addClass("btn-outline-dark");
+			$(this).html("<i class=\"bi bi-pencil-square\"></i>");
+			timeCount = $("#timerSet").val();
+			$("#timerShow").html(timeCount);
+			// console.log(timeCount);
+		}
+	});
+	$("#timerBegin").on("click",function(){
+		if(isCounting==0) {
+			isCounting = 1;
+			timerBegin(timeCount, 5);
+			$("#timerBegin").html("<i class=\"bi bi-arrow-clockwise\"></i>");
+			$("#timerBegin").removeClass("btn-success");
+			$("#timerBegin").addClass("btn-danger");
+		}else{
+			$("#timerBegin").html("<i class=\"bi bi-play\"></i>");
+			$("#timerBegin").removeClass("btn-danger");
+			$("#timerBegin").addClass("btn-success");
+			timerReset();
+		}
+	});
+	// $("#timerReset").on("click",function(){
+	//     $("#timerBegin").removeAttr("disabled");
+	//     $("#timerBegin").html("<i class=\"bi bi-play\"></i>");
+	//     $("#timerBegin").removeClass("btn-warning");
+	//     $("#timerBegin").addClass("btn-success");
+	//     timerReset();
+	// });
+	function timerBegin(time, alarmTime){//倒计时时长、变红时长
+
+		var t = parseInt(time)-1;
+		const redTime = parseInt(alarmTime);
+		countDown = setInterval(function () {
+			$("#timerShow").html(t);
+			if (t == 0) {
+				// doing some alarm;
+				clearInterval(countDown);
+				isCounting = 0;
+				return 0;
+			}
+			if (t == redTime){
+				$("#timerShow").css("color","red");//样式变换
+			}
+			t--;
+		}, 1000);
+	}
+	function timerReset(){
+		clearInterval(countDown);
+		$("#timerShow").css("color","black");
+		$("#timerShow").html(timeCount);
+	}
+
 });
--- resources/views/game/AnnoyingUfo.blade.php
+++ resources/views/game/AnnoyingUfo.blade.php
@@ -2,6 +2,7 @@
 <html lang="zh-CN">
 <head>
 	<title>恼人的UFO - THParty.Fun</title>
+	<meta charset="UTF-8">
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
@@ -11,24 +12,53 @@
 <div class="container my-5">
 	<div class="row">
 		<div class="col-sm-12 col-lg-6">
-			<h1 class="display-1">正在加载中...</h1>
+			<div class="row" id="timerDiv">
+				<div class="col-7" style="text-align:center">
+					<h1 class="display-1" id="timerShow">15</h1>
+				</div>
+				<div class="col-5">
+					<div class="row">
+						<div class="col-4"><input type="number" class="form-control" value="15" min="0" id="timerSet"></div>
+						<div class="col-8">
+							<button id="timerChange" class="btn btn-outline-dark" setting="1"><i class="bi bi-pencil-square"></i></button>
+							<button class="btn btn-success" id="timerBegin"><i class="bi bi-play"></i></button>
+							<!--                            <button id="timerFinishSet" class="btn btn-outline-dark"><i class="bi bi-check"></i></button>-->
+							<!--                            <button id="timerReset" class="btn btn-outline-dark"><i class="bi bi-arrow-clockwise"></i></button>-->
+						</div>
+
+					</div>
+					<div class="row">
+						<div class="col-4"></div>
+						<div class="col-8">
+							<br>
+
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="row"><h1 class="display-1" id="linesText">正在加载中...</h1></div>
 		</div>
 		<div class="col-sm-12 col-lg-6 my-sm-3">
 			<div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
-				<a href="/" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+				<a href="#" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
 					<span class="fs-5 fw-semibold">队伍</span>
 				</a>
 
 				<div class="list-group list-group-flush border-bottom scrollarea" id="groupList">
 					<div class="list-group-item row py-3 lh-tight" id="groupItem1">
 						<div class="row">
-							<div class="col-8"><input type="text" class="groupName form-control" id="groupName1" placeholder="队伍名称..."></div>
-							<div class="groupCount col-4">
+							<div class="col-8"><input type="text" class="groupName form-control" id="groupName1" placeholder="队伍名称..." tid="1"></div>
+							<div class="groupCount col-3">
 								<button class="btn scoreMinus btn-danger" id="scoreMinus1" tid="1"><h5 class="d-inline">-</h5></button>
 								<span><b class="score" id="score1">0</b></span>
 								<button class="btn scoreAdd btn-success" id="scoreAdd1" tid="1"><h5 class="d-inline">+</h5></button>
 							</div>
+							<div class="col-1">
+								<button class="btn deleteGroup" id="deleteGroup1" tid="1"><i class="bi bi-x-circle"></i></button>
+							</div>
 						</div>
+
 					</div>
 
 
@@ -42,9 +72,6 @@
 			</div>
 		</div>
 	</div>
-
 </div>
-
 </body>
 </html>
-/
