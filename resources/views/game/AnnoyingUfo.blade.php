32aadf6cd9f7cf532097c29cb8467478858d602e
李泊桥 <13693338025@163.com>
2023-04-15 23:12:41 +0800

    Update AnnoyingUfo.blade.php

--- resources/views/game/AnnoyingUfo.blade.php
+++ resources/views/game/AnnoyingUfo.blade.php
@@ -16,6 +16,7 @@
 	</style>
 </head>
 <body>
+
 <div class="container my-5">
 	<div class="row">
 		<div class="col-sm-12 col-lg-6">
@@ -52,7 +53,7 @@
 			</div>
 			<div class="row"><h1 class="display-1" id="linesText">等待开始...</h1></div>
 		</div>
-		<div class="col-sm-12 col-lg-6 my-sm-3">
+		<div class="col-sm-12 col-lg-6 my-sm-3" id="part2">
 			<div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
 				<div class="row">
 					<div class="col-12">
@@ -60,8 +61,11 @@
 							<span class="fs-3 fw-semibold">队伍 &nbsp</span><button id="groupReset" class="btn"><i class="bi-arrow-clockwise"></i></button>
 						</a>
 					</div>
+				</div>
+
+
 				<div class="list-group list-group-flush border-bottom scrollarea" id="groupList">
-					<div class="list-group-item py-3 lh-tight groupItem d-flex" id="groupItem1" score="3">
+					<div class="list-group-item py-3 lh-tight groupItem" id="groupItem1" score="3">
 						<div class="row d-flex">
 							<div class="col-6 d-flex"><input type="text" class="groupName form-control input-large input align-items-center" id="groupName1" placeholder="队伍名称..." tid="1"></div>
 							<div class="groupCount col-5">
@@ -107,103 +111,8 @@
 			</div>
 		</div>
 	</div>
+
 </div>
 
-	<div class="container my-5">
-		<div class="row">
-			<div class="col-sm-12 col-lg-6">
-				<div class="row" id="timerDiv">
-					<div class="col-2">
-						<a href="javascript:" class="btn btn-primary" onClick="javascript :history.back(-1);">返回</a>
-					</div>
-					<div class="col-5" style="text-align:center">
-
-						<h1 class="display-1 d-inline" id="timerShow">15</h1>
-					</div>
-					<div class="col-5">
-						<div class="row">
-
-							<div class="col-4">
-								<div class="row p-1"><button id="timerChange" class="btn btn-outline-dark btn-lg" setting="1"><i class="bi bi-pencil-square"></i></button>
-								</div>
-								<div class="row p-1"><button class="btn btn-success btn-lg" id="timerBegin"><i class="bi bi-play"></i></button>
-								</div>
-								<!--                            <button id="timerFinishSet" class="btn btn-outline-dark"><i class="bi bi-check"></i></button>-->
-								<!--                            <button id="timerReset" class="btn btn-outline-dark"><i class="bi bi-arrow-clockwise"></i></button>-->
-							</div>
-							<div class="col-8"><div class="row"><div class="col-lg-9 col-xl-7 col-8"><input type="number" class="form-control" value="15" min="0" id="timerSet"></div></div></div>
-						</div>
-						<div class="row">
-							<div class="col-4"></div>
-							<div class="col-8">
-								<br>
-
-							</div>
-						</div>
-					</div>
-
-				</div>
-				<div class="row"><h1 class="display-1" id="linesText">正在加载中...</h1></div>
-			</div>
-			<div class="col-sm-12 col-lg-6 my-sm-3">
-				<div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
-					<div class="row">
-						<div class="col-12">
-							<a href="#" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-								<span class="fs-3 fw-semibold">队伍 &nbsp</span><button id="groupReset" class="btn"><i class="bi-arrow-clockwise"></i></button>
-							</a>
-						</div>
-					</div>
-
-
-					<div class="list-group list-group-flush border-bottom scrollarea" id="groupList">
-						<div class="list-group-item py-3 lh-tight groupItem" id="groupItem1" score="3">
-							<div class="row d-flex">
-								<div class="col-6 d-flex"><input type="text" class="groupName form-control input-large input align-items-center" id="groupName1" placeholder="队伍名称..." tid="1"></div>
-								<div class="groupCount col-5">
-									<div class="row">
-										<div class="col-8">
-											<div class="row">
-												<div class="col-4 p-0">
-													<img src="./assets/img/full.png" tid="1" imgN="1" width="100%">
-												</div>
-												<div class="col-4 p-0">
-													<img src="./assets/img/full.png" tid="1" imgN="2" width="100%">
-												</div>
-												<div class="col-4 p-0">
-													<img src="./assets/img/full.png" tid="1" imgN="3" width="100%">
-												</div>
-											</div>
-										</div>
-										<div class="col-4 d-flex align-items-center">
-											<button class="btn btn-lg btn-danger scoreAdd" id="scoreAdd1" tid="1">O</button>
-										</div>
-									</div>
-									<!--                            <div>--><!--已经弃用的设计-->
-									<!--                                <button class="btn scoreMinus btn-danger" id="scoreMinus1" tid="1"><h5 class="d-inline">-</h5></button>-->
-									<!--                                <span><b class="score" id="score1">0</b></span>-->
-									<!--                                <button class="btn scoreAdd btn-success" id="scoreAdd1" tid="1"><h5 class="d-inline">+</h5></button>-->
-									<!--                            </div>-->
-								</div>
-								<div class="col-1 align-items-center d-flex">
-									<button class="btn deleteGroup" id="deleteGroup1" tid="1"><i class="bi bi-x-circle"></i></button>
-								</div>
-							</div>
-
-						</div>
-
-
-						<div class="list-group-item py-3 lh-tight">
-							<button class="btn col-12 btn-info" id="addGroup">
-								<b>+</b>
-							</button>
-						</div>
-
-					</div>
-				</div>
-			</div>
-		</div>
-
-	</div>
 </body>
 </html>
